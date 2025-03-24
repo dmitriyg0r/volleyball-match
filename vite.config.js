ef801eb961d7fad79d7c8acde86c29c77ff84a68
dmitriyg0r <62027285+dmitriyg0r@users.noreply.github.com>
2025-03-24 22:50:28 +0300

    6

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/volleyball-match/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
+  }
 })
